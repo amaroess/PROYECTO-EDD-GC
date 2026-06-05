69d39b556374fd8111a14ee20d8ed764480c4255
Amaro Escobar <amaro.escobar.b@mail.pucv.cl>
2026-06-05 15:14:42 -0400

    Se incluyen TDAS a utilizar

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
-// hola
-
-
+#include <stdio.h>
+#include "tdas/list.h" // TDA LISTA
+#include "tdas/map.h" // TDA MAPA
 int main()
 {
-    puts("BIENVENIDO A TU GESTOR DE CONTRASEÑAS");
+    puts("-----BIENVENIDO A TU GESTOR DE CONTRASEÑAS-----\n");
     return 0;   
 }
